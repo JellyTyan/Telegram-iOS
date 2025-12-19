bdb29280e94c6d1bed1fe28f5655ffa60702561d
Kylmakalle <iceman9831@gmail.com>
2025-12-19 22:45:26 +0200

    NY

--- submodules/WallpaperBackgroundNode/Sources/WallpaperBackgroundNode.swift
+++ submodules/WallpaperBackgroundNode/Sources/WallpaperBackgroundNode.swift
@@ -1,3 +1,4 @@
+import SGSimpleSettings
 import Foundation
 import UIKit
 import AsyncDisplayKit
@@ -1020,6 +1021,10 @@ public final class WallpaperBackgroundNodeImpl: ASDisplayNode, WallpaperBackgrou
         }
     }
     
+    // MARK: Swiftgram
+    private var NYNode: WallpaperNYNode?
+    //
+
     private var imageContentMode: UIView.ContentMode {
         didSet {
             self.contentNode.contentMode = self.imageContentMode
@@ -1690,7 +1695,9 @@ public final class WallpaperBackgroundNodeImpl: ASDisplayNode, WallpaperBackgrou
                 
         if isFirstLayout && !self.frame.isEmpty {
             self.updateScale()
+            /* MARK: Swiftgram */ if SGSimpleSettings.shared.isNYEnabled && self.NYNode == nil { let nYNode = WallpaperNYNode(); self.addSubnode(nYNode); self.NYNode = nYNode }
         }
+        /* MARK: Swiftgram */ self.NYNode?.frame = CGRect(origin: CGPoint(), size: size); self.NYNode?.updateLayout(size: size)
     }
 
     private var isAnimating = false
